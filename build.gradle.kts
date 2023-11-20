75829847bce3c91f8221bdfca817c5f22bac5b45
Rafael Vigo <rvigo07+github@gmail.com>
2023-11-20 15:51:46 -0300

    feat: includes detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
     kotlin("jvm") version "1.9.10"
     kotlin("plugin.spring") version "1.9.10"
     kotlin("plugin.jpa") version "1.9.10"
@@ -40,6 +42,9 @@ dependencies {
     // test
     testImplementation("io.mockk:mockk:1.13.8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // detekt
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -51,4 +56,27 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+configurations.detekt {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.jetbrains.kotlin") {
+            useVersion("1.9.0")
+        }
+    }
+}
+
+detekt {
+    config.from("./detekt-config.yaml")
+    autoCorrect = true
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        xml.required.set(false)
+        txt.required.set(false)
+        md.required.set(false)
+        sarif.required.set(false)
+        html.required.set(true)
+    }
 }
